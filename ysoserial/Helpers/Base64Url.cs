d2ff314699d2a9519ae04d2458de1f58fb6b3230
testanull <jangpro.net@gmail.com>
2021-06-11 16:05:09 +0700

    Update new line

--- ysoserial/Helpers/Base64Url.cs
+++ ysoserial/Helpers/Base64Url.cs
@@ -10,7 +10,7 @@ namespace ysoserial.Helpers
         {
             string b64str = Convert.ToBase64String(str);
             byte[] bb1 = Encoding.UTF8.GetBytes(b64str);
-            string strOut = HttpServerUtility.UrlTokenEncode(bb1);
+            string strOut = HttpServerUtility.UrlTokenEncode(bb1).Replace("\n", "");
             return strOut;
         }
         public static byte[] Base64Decode(string str) => Convert.FromBase64String(Encoding.UTF8.GetString(HttpServerUtility.UrlTokenDecode(str)));
